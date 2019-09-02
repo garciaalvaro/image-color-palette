6f138ffd85e16c9eca23b1ae716cde0ef2cecf5f
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2019-09-02 23:28:28 +0200

    Updated ColorRow from class to functional Component to make use of hooks.

--- src/Components/ColorRow/ColorRow.tsx
+++ src/Components/ColorRow/ColorRow.tsx
@@ -3,112 +3,63 @@ import { store_slug } from "utils/data";
 import { Div } from "utils/Components";
 import { ColorRowButton } from "./ColorRowButton";
 
-interface WithStateProps {
-	color_hex: "";
-	color_type: "light" | "dark";
-	scheme_colors: string[];
-	scheme_colors_type: ("light" | "dark")[];
-}
-
 interface WithSelectProps extends Pick<State, "color_scheme"> {}
 
 interface OwnProps {
 	color: string;
 }
 
-interface Props
-	extends WithSelectProps,
-		WithStateProps,
-		SetStateProp,
-		OwnProps {}
-
-const { Component } = wp.element;
-const { withState, compose } = wp.compose;
+const { useState, useEffect } = wp.element;
 const { withSelect } = wp.data;
 
-export const ColorRow: React.ComponentType<OwnProps> = compose([
-	withState<WithStateProps>({
-		color_hex: "",
-		color_type: "light",
-		scheme_colors: [],
-		scheme_colors_type: []
-	}),
-	withSelect<WithSelectProps>(select => ({
-		color_scheme: select(store_slug).getColorScheme()
-	}))
-])(
-	class extends Component<Props> {
-		componentDidMount() {
-			const { color_scheme, color, setState } = this.props;
+export const ColorRow: React.ComponentType<OwnProps> = withSelect<
+	WithSelectProps,
+	OwnProps
+>(select => ({
+	color_scheme: select(store_slug).getColorScheme()
+}))(props => {
+	const { color, color_scheme } = props;
+	const [scheme_colors, setSchemeColors] = useState<string[]>([]);
+	const [scheme_colors_type, setSchemeColorsType] = useState<
+		("light" | "dark")[]
+	>([]);
+	const [color_hex, setColorHex] = useState<string>("");
+	const [color_type, setColorType] = useState<"light" | "dark">("light");
 
-			const {
-				scheme_colors,
-				scheme_colors_type,
-				color_hex,
-				color_type
-			} = generateColorsScheme(color_scheme, color);
+	useEffect(() => {
+		const {
+			scheme_colors,
+			scheme_colors_type,
+			color_hex,
+			color_type
+		} = generateColorsScheme(color_scheme, color);
 
-			setState({
-				scheme_colors,
-				scheme_colors_type,
-				color_hex,
-				color_type
-			});
-		}
+		setSchemeColors(scheme_colors);
+		setSchemeColorsType(scheme_colors_type);
+		setColorHex(color_hex);
+		setColorType(color_type);
+	}, [color, color_scheme]);
 
-		componentDidUpdate(prev_props: Props) {
-			const { color_scheme, color, setState } = this.props;
-
-			if (
-				color_scheme !== prev_props.color_scheme ||
-				color !== prev_props.color
-			) {
-				const {
-					scheme_colors,
-					scheme_colors_type,
-					color_hex,
-					color_type
-				} = generateColorsScheme(color_scheme, color);
-
-				setState({
-					scheme_colors,
-					scheme_colors_type,
-					color_hex,
-					color_type
-				});
-			}
-		}
-
-		render() {
-			const {
-				color_hex,
-				scheme_colors,
-				color_type,
-				scheme_colors_type
-			} = this.props;
-
-			return (
-				<Div className="color_row">
-					<ColorRowButton
-						label={color_hex}
-						button_extra_className="main_color"
-						color={color_hex}
-						color_type={color_type}
-					/>
-					{!!scheme_colors.length && (
-						<Div className="scheme_colors">
-							{scheme_colors.map((color, index) => (
-								<ColorRowButton
-									key={index}
-									button_extra_className="scheme_color"
-									color={color}
-									color_type={scheme_colors_type[index]}
-								/>
-							))}
-						</Div>
-					)}
+	return (
+		<Div className="color_row">
+			<ColorRowButton
+				label={color_hex}
+				button_extra_className="main_color"
+				color={color_hex}
+				color_type={color_type}
+			/>
+			{!!scheme_colors.length && (
+				<Div className="scheme_colors">
+					{scheme_colors.map((color, index) => (
+						<ColorRowButton
+							key={index}
+							button_extra_className="scheme_color"
+							color={color}
+							color_type={scheme_colors_type[index]}
+						/>
+					))}
 				</Div>
-			);
-		}
-	}
-);
+			)}
+		</Div>
+	);
+});
