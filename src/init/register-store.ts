cebbc7041d7344bb3ce9f2480617896c457ece10
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2021-05-08 18:08:07 +0200

    Removed use of createReduxStore

--- src/init/register-store.ts
+++ src/init/register-store.ts
@@ -1,44 +1,20 @@
-import {
-	createReduxStore,
-	register,
-	registerStore,
-	Store,
-} from "@wordpress/data";
+import { registerStore } from "@wordpress/data";
 
 import { store_slug } from "@/utils";
 import { reducer } from "@/store/reducer";
 import { actions } from "@/store/actions";
 import { selectors } from "@/store/selectors";
 
-let store: Store<State>;
+export const store = registerStore<State>(store_slug, {
+	// @ts-expect-error TODO
+	reducer,
+	// @ts-expect-error TODO
+	actions,
+	selectors,
 
-if (createReduxStore !== undefined) {
-	store = createReduxStore(store_slug, {
-		reducer,
-		actions,
-		selectors,
-		persist: [
-			"color_palette_length",
-			"color_scheme",
-			"color_distance_equation",
-		],
-	});
-
-	register(store);
-} else {
-	store = registerStore<State>(store_slug, {
-		// @ts-expect-error TODO
-		reducer,
-		// @ts-expect-error TODO
-		actions,
-		selectors,
-
-		persist: [
-			"color_palette_length",
-			"color_scheme",
-			"color_distance_equation",
-		],
-	});
-}
-
-export { store };
+	persist: [
+		"color_palette_length",
+		"color_scheme",
+		"color_distance_equation",
+	],
+});
